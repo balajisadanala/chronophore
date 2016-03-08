dee046617bbdab2beeeb1d32ad856615c3ad9941
Amin Mesbah <mesbah.amin@gmail.com>
2016-03-08 12:34:20 -0800

    update unit tests to cover Entry member functions

--- tests.py
+++ tests.py
@@ -1,34 +1,35 @@
 import os
-import signin
+import timebook
 import unittest
 
-class FileTest(unittest.TestCase):
-    
-    entry = signin.Entry("2016-02-17", "889870966", "10:45", "13:30")
+
+class EntryTest(unittest.TestCase):
     test_file = "./test.json"
-    expected = """\
-{
-    "Date": "2016-02-17",
-    "In": "10:45",
-    "Out": "13:30",
-    "Student ID": "889870966"
-}"""
+    example_file = "./example.json"
 
     def setUp(self):
         open(self.test_file, 'a')
-        
+
     def test_save_entry(self):
-        signin.save_entry(self.entry, timesheet_file=self.test_file)
+        """Create an entry with values, then save them to a file"""
+        entry = timebook.Entry("2016-02-17", "889870966", "10:45", "13:30")
+        entry.save_entry(timesheet_file=self.test_file)
+        with open(self.example_file, 'r') as f:
+            expected = f.read()
         with open(self.test_file, 'r') as f:
             result = f.read()
-        self.assertIn(self.expected, result)
+        self.assertEqual(expected, result)
 
     def test_load_entry(self):
-        pass
-        
-        
+        """Create an empty entry, then load values from a file"""
+        entry = timebook.Entry()
+        entry.load_entry(timesheet_file=self.example_file)
+        result = entry.data
+        expected = {'In': '10:45', 'Date': '2016-02-17', 'Student ID': '889870966', 'Out': '13:30'}
+        self.assertEqual(expected, result)
+
     def tearDown(self):
         os.remove(self.test_file)
-        
+
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
