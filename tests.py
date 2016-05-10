2351eb7347a8bb513a3fcd37b4b12ee2732b641b
Amin Mesbah <mesbah.amin@gmail.com>
2016-05-09 18:29:33 -0700

    add method to load existing entries from the timesheet

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+import json
 import os
 import timebook
 import unittest
@@ -8,7 +9,7 @@ from datetime import datetime
 class EntryTest(unittest.TestCase):
     @unittest.mock.patch(
         'timebook.Entry._get_current_datetime',
-        return_value = datetime(2016, 5, 9, 15, 43, 41, 433749)
+        return_value=datetime(2016, 5, 9, 15, 43, 41, 433749)
     )
     def test_create_entry(self, get_current_datetime):
         e = timebook.Entry(user_id="882369423")
@@ -18,7 +19,7 @@ class EntryTest(unittest.TestCase):
 
     @unittest.mock.patch(
         'timebook.Entry._get_current_datetime',
-        return_value = datetime(2016, 5, 9, 17, 30, 17, 433749)
+        return_value=datetime(2016, 5, 9, 17, 30, 17, 433749)
     )
     def test_sign_out(self, get_current_datetime):
         e = timebook.Entry(user_id="882369423")
@@ -54,13 +55,13 @@ class TimesheetTest(unittest.TestCase):
         }
 
     @unittest.mock.patch(
-        'timebook.Timesheet._make_index',
+        'timebook.Entry._make_index',
         return_value='3b27d0f8-3801-4319-398f-ace18829d150'
     )
-    def test_add_entry(self, make_index):
+    def test_save_entry(self, make_index):
         """Add an entry to an empty timesheet."""
-        e = timebook.Entry("2016-02-17", "889870966", "10:45", "13:30")
-        self.t.add_entry(e)
+        e = timebook.Entry("889870966", "2016-02-17", "10:45", "13:30")
+        self.t.save_entry(e)
         expected_sheet = {
             "3b27d0f8-3801-4319-398f-ace18829d150": {
                 "In": "10:45",
@@ -74,9 +75,10 @@ class TimesheetTest(unittest.TestCase):
     def test_remove_entry(self):
         """Remove an entry from a timesheet with multiple entries."""
         # dicts are mutable; passed by reference unless explicitly copied:
+        index = "1f4f10a4-b0c6-43bf-94f4-9ce6e3e204d2"
         self.t.sheet = dict(self.example_sheet)
-        self.t.remove_entry("1f4f10a4-b0c6-43bf-94f4-9ce6e3e204d2")
-        self.assertFalse("1f4f10a4-b0c6-43bf-94f4-9ce6e3e204d2" in self.t.sheet)
+        self.t.remove_entry(index)
+        self.assertFalse(index in self.t.sheet)
 
     def test_load_sheet(self):
         """Load a sheet from a file."""
@@ -93,7 +95,7 @@ class TimesheetTest(unittest.TestCase):
             result = f.read()
         with open(self.example_file, 'r') as e:
             expected_file = e.read()
-        # NOTE: Watch out for trailing newline after editing file 
+        # NOTE: Watch out 1f4f10a4-b0c6-43bf-94f4-9ce6e3e204d2for trailing newline after editing file
         self.assertMultiLineEqual(result, expected_file)
         os.remove(self.test_file)
 
@@ -107,6 +109,20 @@ class TimesheetTest(unittest.TestCase):
         ]
         self.assertEqual(entries, expected_entries)
 
+    def test_load_entry(self):
+        """Initialize an entry object with data from the timesheet"""
+        self.t.sheet = self.example_sheet
+        entry = self.t.load_entry("1f4f10a4-b0c6-43bf-94f4-9ce6e3e204d2")
+        expected_entry = {
+            "1f4f10a4-b0c6-43bf-94f4-9ce6e3e204d2": {
+                "Date": "2016-02-17",
+                "In": "10:45",
+                "Out": "13:30",
+                "Student ID": "889870966",
+            },
+        }
+        self.assertEqual(repr(entry), json.dumps(expected_entry, indent=4, sort_keys=True))
+
     def test_find_signed_in(self):
         """Find all entries of people that are currently signed in
         (there is a time_in value, but not a time_out value).
@@ -115,28 +131,28 @@ class TimesheetTest(unittest.TestCase):
         self.t._update_signed_in()
         expected_entries = ["2ed2be60-693a-44fe-adc1-2803a674ec9b"]
         self.assertEqual(self.t.signedin, expected_entries)
-        
+
     @unittest.mock.patch(
-        'timebook.Timesheet._make_index',
+        'timebook.Entry._make_index',
         return_value='3b27d0f8-3801-4319-398f-ace18829d150'
     )
     def test_sign_in(self, make_index):
         """User signs in, and they are added to the list of currently
         signed in users.
         """
-        e = timebook.Entry("2016-02-17", "889870966", "10:45")
-        self.t.add_entry(e)
+        e = timebook.Entry("889870966", "2016-02-17", "10:45")
+        self.t.save_entry(e)
         self.t._update_signed_in()
         self.assertIn("3b27d0f8-3801-4319-398f-ace18829d150", self.t.signedin)
-        
+
     def test_sign_out(self):
         """User signs out, and they are removed from the list of currently
         signed in users.
         """
         self.t.sheet = dict(self.example_sheet)
-        e = timebook.Entry("2016-02-17", "889870966", "10:45", "13:30")
+        e = timebook.Entry("889870966", "2016-02-17", "10:45", "13:30")
         index = "2ed2be60-693a-44fe-adc1-2803a674ec9b"
-        self.t.add_entry(e, index)
+        self.t.save_entry(e, index)
         self.t._update_signed_in()
         self.assertNotIn(index, self.t.signedin)
 
--- timebook.py
+++ timebook.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 # TODO:
 # - [ ] user-facing command line interface
 # - [ ] exceptions/validation
+# - [ ] protect agains accidental file overwrite
 # - [ ] command line mvp
 # - [ ] load timesheet file partially into memory if possible
 # - [ ] auto-completion
@@ -13,10 +14,11 @@ from datetime import datetime, timedelta
 
 class Entry():
     """Contains all data for a single entry"""
-    def __init__(self, date=None, user_id=None, time_in=None, time_out=None):
+    def __init__(self, user_id=None, date=None, time_in=None,
+                 time_out=None, index=None):
         now = self._get_current_datetime()
         if date is None:
-            self.date = datetime.strftime(now, "%Y-%m-%d") 
+            self.date = datetime.strftime(now, "%Y-%m-%d")
         else:
             self.date = date
         if user_id is None:
@@ -24,22 +26,39 @@ class Entry():
         else:
             self.user_id = user_id
         if time_in is None:
-            self.time_in = datetime.strftime(now, "%H:%M:%S") 
+            self.time_in = datetime.strftime(now, "%H:%M:%S")
         else:
             self.time_in = time_in
         if time_out is None:
             self.time_out = ""
         else:
             self.time_out = time_out
+        if index is None:
+            self.index = self._make_index()
+        else:
+            self.index = index
+
+    def __repr__(self):
+        data = {}
+        data['Date'] = self.date
+        data['Student ID'] = self.user_id
+        data['In'] = self.time_in
+        data['Out'] = self.time_out
+        entry = {}
+        entry[self.index] = data
+        return json.dumps(entry, indent=4, sort_keys=True)
 
     def _get_current_datetime(self):
         """Serves as a mockable reference to datetime.today()"""
         return datetime.today()
 
+    def _make_index(self):
+        return str(uuid.uuid4())
+
     def sign_out(self):
         now = self._get_current_datetime()
         self.time_out = datetime.strftime(now, "%H:%M:%S")
-        
+
 
 class Timesheet():
     """Contains multiple entries"""
@@ -55,30 +74,43 @@ class Timesheet():
         data['Out'] = entry.time_out
         return data
 
-    def _make_index(self):
-        return str(uuid.uuid4())
-
     def _update_signed_in(self):
         self.signedin = [k for k, v in self.sheet.items() if v['Out'] == ""]
 
     def find_entry(self, search_term):
+        # NOTE(amin): use a tuple instead of a list here?
         entries = [
             k for k, v in self.sheet.items() if search_term in str(v.items())
         ]
         return sorted(entries)
 
-    def add_entry(self, entry, index=None):
-        if index == None:
-            index = self._make_index()
+    def load_entry(self, index):
+        entry = Entry(
+                    self.sheet[index]['Student ID'],
+                    self.sheet[index]['Date'],
+                    self.sheet[index]['In'],
+                    self.sheet[index]['Out'],
+                    index
+                )
+        return entry
+
+    def save_entry(self, entry, index=None):
+        if index is None:
+            index = entry.index
+        else:
+            index = index
         entry_data = self._make_dict(entry)
         self.sheet[index] = entry_data
+        self._update_signed_in()
 
     def remove_entry(self, index):
         del self.sheet[str(index)]
+        self._update_signed_in()
 
     def load_sheet(self, timesheet_file='./timesheet.json'):
         with open(timesheet_file, 'r') as f:
             self.sheet = json.load(f)
+        self._update_signed_in()
 
     def save_sheet(self, timesheet_file='./timesheet.json'):
         with open(timesheet_file, 'w') as f:
@@ -86,15 +118,21 @@ class Timesheet():
 
 
 def main():
-    x = Entry("2016-02-17", "889870966", "10:45", "13:30")
     t = Timesheet()
-    t.add_entry(x)
-    t.add_entry(x)
-    t.add_entry(x)
-    t.add_entry(x)
-    t.add_entry(x)
+    x = Entry("889870966")
+    y = Entry("883830333")
+    t.save_entry(x)
+    entries = t.find_entry("889870966")
+    for index in entries:
+        print(index)
+        e = t.load_entry(index)
+        print(e.index)
+        print(e.user_id)
+        print(e.date)
+        print(e.time_in)
+        print(e.time_out)
+    t.save_entry(y)
     t.save_sheet()
-    print(t.find_entry("10:45"))
 
 
 if __name__ == '__main__':
