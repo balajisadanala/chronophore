a50d3099cec5652d5032552f15bdf805856e1224
Amin Mesbah <mesbah.amin@gmail.com>
2016-07-19 16:02:30 -0700

    enclose gui in a class called TimebookUI

--- timebook/gui.py
+++ timebook/gui.py
@@ -2,56 +2,71 @@ from timebook import timebook
 import tkinter
 from tkinter import ttk, N, S, E, W
 
-t = timebook.Timesheet()
+class TimebookUI():
+    """Simple Tkinter GUI for timebook:
+            - Entry for user id input
+            - Button to sign in or out
+            - List of currently signed in users
+    """
 
-def sign_in_out(*args):
-    user_id = ent_id.get()
-    timebook.sign(t, user_id)
-    t.save_sheet()
-    signed_in.set('\n'.join(t.signed_in))
+    def __init__(self, parent, timesheet):
+        self.t = timesheet
 
-# root window
-root = tkinter.Tk()
-root.title("STEM Sign In")
-content = ttk.Frame(root, padding=(5,5,10,10))
+        parent.title("STEM Sign In")
+        self.content = ttk.Frame(parent, padding=(5,5,10,10))
+        self.signed_in = tkinter.StringVar()
 
-signed_in = tkinter.StringVar(root)
+        # widgets
+        self.frm_signedin = ttk.Frame(self.content, borderwidth=5, relief="sunken", width=200, height=100)
+        self.lbl_signedin = ttk.Label(self.content, text="Currently Signed In:")
+        self.lbl_signedin_list = ttk.Label(self.frm_signedin, textvariable=self.signed_in)
+        self.lbl_welcome = ttk.Label(self.content, text="Welcome to the STEM Learning Center!")
+        self.lbl_id = ttk.Label(self.content, text="Enter Student ID")
+        self.user_id = tkinter.StringVar()
+        self.ent_id = ttk.Entry(self.content, textvariable=self.user_id)
+        self.btn_sign = ttk.Button(self.content, text="Sign In/Out", command=self.sign_in_out)
 
-# widgets
-frm_signedin = ttk.Frame(content, borderwidth=5, relief="sunken", width=200, height=100)
-lbl_signedin = ttk.Label(content, text="Currently Signed In:")
-lbl_signedin_list = ttk.Label(frm_signedin, textvariable=signed_in)
-lbl_welcome = ttk.Label(content, text="Welcome to the STEM Learning Center!")
-lbl_id = ttk.Label(content, text="Enter Student ID")
-user_id = tkinter.StringVar()
-ent_id = ttk.Entry(content, textvariable=user_id)
-btn_sign = ttk.Button(content, text="Sign In/Out", command=sign_in_out)
+        # assemble grid
+        self.content.grid(column=0, row=0, sticky=(N, S, E, W))
+        self.lbl_signedin.grid(column=0, row=0, pady=0, sticky=(W))
+        self.frm_signedin.grid(column=0, row=1, columnspan=1, rowspan=3, sticky=(N, S, E, W))
+        self.lbl_signedin_list.grid(column=0, row=0, columnspan=1, rowspan=3)
+        self.lbl_welcome.grid(column=2, row=1, columnspan=1)
+        # TODO(amin): figure out why lbl_id and btn_sign wiggle
+        # when lbl_signedin_list updates
+        self.lbl_id.grid(column=2, row=2, columnspan=1, sticky=(N))
+        # TODO(amin): add select all shortcuts to this entry
+        self.ent_id.grid(column=2, row=2, columnspan=1, sticky=(E, W))
+        self.btn_sign.grid(column=2, row=2, columnspan=1, sticky=(S))
 
-# assemble grid
-content.grid(column=0, row=0, sticky=(N, S, E, W))
-lbl_signedin.grid(column=0, row=0, pady=0, sticky=(W))
-frm_signedin.grid(column=0, row=1, columnspan=1, rowspan=3, sticky=(N, S, E, W))
-lbl_signedin_list.grid(column=0, row=0, columnspan=1, rowspan=3)
-lbl_welcome.grid(column=2, row=1, columnspan=1)
-lbl_id.grid(column=2, row=2, columnspan=1, sticky=(N))
-# TODO(amin): add select all shortcuts to this entry
-ent_id.grid(column=2, row=2, columnspan=1, sticky=(E, W))
-btn_sign.grid(column=2, row=2, columnspan=1, sticky=(S))
+        # resize weights
+        parent.columnconfigure(0, minsize=400, weight=1)
+        parent.rowconfigure(0, minsize=200, weight=1)
+        self.content.columnconfigure(0, weight=1)
+        self.content.columnconfigure(1, weight=3)
+        self.content.columnconfigure(2, weight=3)
+        self.content.columnconfigure(3, weight=3)
+        self.content.rowconfigure(0, weight=0)
+        self.content.rowconfigure(1, weight=3)
+        self.content.rowconfigure(2, minsize=100, weight=1)
+        self.content.rowconfigure(3, weight=3)
 
-# resize weights
-root.columnconfigure(0, minsize=400, weight=1)
-root.rowconfigure(0, minsize=200, weight=1)
-content.columnconfigure(0, weight=1)
-content.columnconfigure(1, weight=3)
-content.columnconfigure(2, weight=3)
-content.columnconfigure(3, weight=3)
-content.rowconfigure(0, weight=0)
-content.rowconfigure(1, weight=3)
-content.rowconfigure(2, minsize=100, weight=1)
-content.rowconfigure(3, weight=3)
+        parent.bind('<Return>', self.sign_in_out)
+        self.ent_id.focus()
 
-ent_id.focus()
+    def sign_in_out(self, *args):
+        user_id = self.ent_id.get()
+        timebook.sign(self.t, user_id)
+        self.t.save_sheet()
+        self.signed_in.set('\n'.join(sorted(
+                [self.t.sheet[i]['User ID'] for i in self.t.signed_in])))
+        self.ent_id.delete(0, 'end')
+        self.ent_id.focus()
 
-root.bind('<Return>', sign_in_out)
 
-root.mainloop()
+if __name__ == '__main__':
+    # Usage example
+    t = timebook.Timesheet()
+    root = tkinter.Tk()
+    ui = TimebookUI(root, t)
+    root.mainloop()
