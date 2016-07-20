1c6280daf4347ad240d8ffff0dc1c8cb62eaf85d
Amin Mesbah <mesbah.amin@gmail.com>
2016-07-20 16:26:54 -0700

    break up long lines

--- timebook/gui.py
+++ timebook/gui.py
@@ -2,6 +2,7 @@ from timebook import timebook
 import tkinter
 from tkinter import ttk, N, S, E, W
 
+
 class TimebookUI():
     """Simple Tkinter GUI for timebook:
             - Entry for user id input
@@ -14,23 +15,48 @@ class TimebookUI():
 
         root = tkinter.Tk()
         root.title("STEM Sign In")
-        self.content = ttk.Frame(root, padding=(5,5,10,10))
+        self.content = ttk.Frame(root, padding=(5, 5, 10, 10))
         self.signed_in = tkinter.StringVar()
 
         # widgets
-        self.frm_signedin = ttk.Frame(self.content, borderwidth=5, relief="sunken", width=200, height=100)
-        self.lbl_signedin = ttk.Label(self.content, text="Currently Signed In:")
-        self.lbl_signedin_list = ttk.Label(self.frm_signedin, textvariable=self.signed_in)
-        self.lbl_welcome = ttk.Label(self.content, text="Welcome to the STEM Learning Center!")
+        self.frm_signedin = ttk.Frame(
+            self.content,
+            borderwidth=5,
+            relief="sunken",
+            width=200,
+            height=100
+        )
+        self.lbl_signedin = ttk.Label(
+            self.content,
+            text="Currently Signed In:"
+        )
+        self.lbl_signedin_list = ttk.Label(
+            self.frm_signedin,
+            textvariable=self.signed_in
+        )
+        self.lbl_welcome = ttk.Label(
+            self.content,
+            text="Welcome to the STEM Learning Center!"
+        )
         self.lbl_id = ttk.Label(self.content, text="Enter Student ID")
+
         self.user_id = tkinter.StringVar()
-        self.ent_id = ttk.Entry(self.content, textvariable=self.user_id)
-        self.btn_sign = ttk.Button(self.content, text="Sign In/Out", command=self.sign_in_out)
+
+        self.ent_id = ttk.Entry(
+            self.content,
+            textvariable=self.user_id)
+        self.btn_sign = ttk.Button(
+            self.content,
+            text="Sign In/Out",
+            command=self.sign_in_out
+        )
 
         # assemble grid
         self.content.grid(column=0, row=0, sticky=(N, S, E, W))
         self.lbl_signedin.grid(column=0, row=0, pady=0, sticky=(W))
-        self.frm_signedin.grid(column=0, row=1, columnspan=1, rowspan=3, sticky=(N, S, E, W))
+        self.frm_signedin.grid(
+            column=0, row=1, columnspan=1, rowspan=3, sticky=(N, S, E, W)
+        )
         self.lbl_signedin_list.grid(column=0, row=0, columnspan=1, rowspan=3)
         self.lbl_welcome.grid(column=2, row=1, columnspan=1)
         # TODO(amin): figure out why lbl_id and btn_sign wiggle
