36d584fe5e1ee0432971a98427daf1c8f6f69bad
Amin Mesbah <mesbah.amin@gmail.com>
2016-08-09 00:53:38 -0700

    Use pytest syntax in unit tests.

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,82 +1,88 @@
 import logging
 import pathlib
-import unittest
-import unittest.mock
+import pytest
 from timebook.controller import Controller
 from timebook.model import Entry, Timesheet
 
 logging.disable(logging.CRITICAL)
 
 
-class ControllerTest(unittest.TestCase):
+@pytest.fixture()
+def timesheet(request):
     test_file = pathlib.Path('.', 'tests', 'test.json')
+
+    def tearDown():
+        if test_file.exists():
+            test_file.unlink()
+    request.addfinalizer(tearDown)
+    return Timesheet(data_file=test_file)
+
+
+class TestController:
     users_file = pathlib.Path('.', 'tests', 'users.json')
     registered_id = "876543210"
     c = Controller(users_file)
 
-    def setUp(self):
-        self.t = Timesheet(data_file=self.test_file)
-
-    def test_detect_duplicates(self):
+    def test_detect_duplicates(self, timesheet):
         duplicate_keys_file = pathlib.Path(
             '.', 'tests', 'duplicate_keys.json'
         )
         lines = ["{", '"key":1234,', '"key":5678', "}"]
         with duplicate_keys_file.open('w') as f:
             f.write('\n'.join(lines))
-        with self.assertRaises(self.c.DuplicateKeysError):
+        with pytest.raises(self.c.DuplicateKeysError):
             self.c._detect_duplicates(duplicate_keys_file)
         duplicate_keys_file.unlink()
 
-    def test_is_valid(self):
-        self.assertFalse(self.c.is_valid("12"))
-        self.assertFalse(self.c.is_valid("1234567890"))
-        self.assertFalse(self.c.is_valid("1234 56789"))
-        self.assertFalse(self.c.is_valid(""))
-        self.assertFalse(self.c.is_valid(" "))
-        self.assertFalse(self.c.is_valid("123abc"))
-        self.assertFalse(self.c.is_valid('\n'))
-        self.assertTrue(self.c.is_valid("123456789"))
-        self.assertTrue(self.c.is_valid(" 123456789"))
-        self.assertTrue(self.c.is_valid("123456789 "))
+    def test_is_valid(self, timesheet):
+        assert not self.c.is_valid("12")
+        assert not self.c.is_valid("1234567890")
+        assert not self.c.is_valid("1234 56789")
+        assert not self.c.is_valid("")
+        assert not self.c.is_valid(" ")
+        assert not self.c.is_valid("123abc")
+        assert not self.c.is_valid('\n')
+        assert self.c.is_valid("123456789")
+        assert self.c.is_valid(" 123456789")
+        assert self.c.is_valid("123456789 ")
 
     def test_is_registered(self):
-        self.assertFalse(self.c.is_registered("888888888"))
-        self.assertTrue(self.c.is_registered(self.registered_id))
+        assert not self.c.is_registered("888888888")
+        assert self.c.is_registered(self.registered_id)
 
     def test_sign_invalid(self):
-        with self.assertRaises(ValueError):
-            self.c.sign(self.t, "1234567890")
+        with pytest.raises(ValueError):
+            self.c.sign(timesheet, "1234567890")
 
-    def test_sign_not_registered(self):
-        with self.assertRaises(self.c.NotRegisteredError):
-            self.c.sign(self.t, "888888888")
+    def test_sign_not_registered(self, timesheet):
+        with pytest.raises(self.c.NotRegisteredError):
+            self.c.sign(timesheet, "888888888")
 
-    def test_sign_duplicates(self):
+    def test_sign_duplicates(self, timesheet):
         """Sign in with multiple instances of being signed in."""
         duplicate_id = self.registered_id
         for _ in range(2):
             e = Entry(duplicate_id)
-            self.t.save_entry(e)
-        with self.assertRaises(self.c.DuplicateEntryError):
-            self.c.sign(self.t, duplicate_id)
+            timesheet.save_entry(e)
+        with pytest.raises(self.c.DuplicateEntryError):
+            self.c.sign(timesheet, duplicate_id)
 
-    @unittest.mock.patch(
-            'timebook.model.Entry.make_index',
-            return_value='3b27d0f8-3801-4319-398f-ace18829d150')
-    def test_sign_in(self, make_index):
+    def test_sign_in(self, monkeypatch, timesheet):
         """Sign in with a new ID."""
         # NOTE(amin): remember that time you spent 40 minutes trying to
         # figure out why this test was failing, only to realize with horror
         # that there is a difference between "882870192" and "882870l92"?
+        def mock_index(Entry):
+            return '3b27d0f8-3801-4319-398f-ace18829d150'
+        monkeypatch.setattr(Entry, 'make_index', mock_index)
         user_id = self.registered_id
-        self.c.sign(self.t, user_id)
-        self.assertEqual(
-            self.t.sheet['3b27d0f8-3801-4319-398f-ace18829d150']['User ID'],
-            user_id
+        self.c.sign(timesheet, user_id)
+        assert(
+            timesheet.sheet['3b27d0f8-3801-4319-398f-ace18829d150']['User ID']
+            == user_id
         )
 
-    def test_sign_out(self):
+    def test_sign_out(self, timesheet):
         """Sign out with an ID that's currently signed in."""
         self.e = Entry(
             user_id=self.registered_id,
@@ -85,11 +91,7 @@ class ControllerTest(unittest.TestCase):
             time_out=None,
             index="2ed2be60-693a-44fe-adc1-2803a674ec9b"
         )
-        self.t.save_entry(self.e)
-        self.c.sign(self.t, self.registered_id)
-        self.assertIsNotNone(self.t.sheet[self.e.index])
-        self.assertNotIn(self.e.index, self.t.signed_in)
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+        timesheet.save_entry(self.e)
+        self.c.sign(timesheet, self.registered_id)
+        assert timesheet.sheet[self.e.index] is not None
+        assert self.e.index not in timesheet.signed_in
--- tests/test_model.py
+++ tests/test_model.py
@@ -3,14 +3,25 @@ import filecmp
 import json
 import logging
 import pathlib
-import unittest
+import pytest
 from datetime import datetime
 from timebook.model import Entry, Timesheet
 
 logging.disable(logging.CRITICAL)
 
 
-class EntryTest(unittest.TestCase):
+@pytest.fixture()
+def timesheet(request):
+    test_file = pathlib.Path('.', 'tests', 'test.json')
+
+    def tearDown():
+        if test_file.exists():
+            test_file.unlink()
+    request.addfinalizer(tearDown)
+    return Timesheet(data_file=test_file)
+
+
+class TestEntry:
 
     def test_create_entry(self):
         """Create an entry with an ID."""
@@ -18,17 +29,17 @@ class EntryTest(unittest.TestCase):
             user_id="882369423",
             now=datetime(2016, 5, 9, 15, 43, 41, 0)
         )
-        self.assertEqual(e.date, "2016-05-09")
-        self.assertEqual(e.time_in, "15:43:41")
-        self.assertEqual(e.time_out, "")
-        self.assertIsNotNone(e.index)
+        assert e.date == "2016-05-09"
+        assert e.time_in == "15:43:41"
+        assert e.time_out == ""
+        assert e.index is not None
 
     def test_repr(self):
         """Test whether the repr of an Entry evaluates to an
         equivalent Entry object.
         """
         e = Entry()
-        self.assertEqual(e, eval(repr(e)))
+        assert e == eval(repr(e))
 
     def test_comparison(self):
         """Test whether '==' and '!=' work properly."""
@@ -39,48 +50,35 @@ class EntryTest(unittest.TestCase):
             time_out="13:30",
             index="1f4f10a4-b0c6-43bf-94f4-9ce6e3e204d2"
         )
-
         equal_entry = entry
-
-        self.assertTrue(entry == equal_entry)
-        self.assertTrue(equal_entry == entry)
-        self.assertFalse(entry != equal_entry)
-        self.assertFalse(equal_entry != entry)
+        assert entry == equal_entry
+        assert equal_entry == entry
+        assert (entry != equal_entry) is False
+        assert (equal_entry != entry) is False
 
     def test_sign_out(self):
         """Create an entry, then sign out of it."""
         e = Entry(user_id="882369423")
         e.sign_out(time=datetime(2016, 5, 9, 17, 30, 17, 0))
-        self.assertEqual(e.time_out, "17:30:17")
+        assert e.time_out == "17:30:17"
 
 
-class TimesheetTest(unittest.TestCase):
-    test_file = pathlib.Path('.', 'tests', 'test.json')
+class TestTimesheet:
+
     example_file = pathlib.Path('.', 'tests', 'example.json')
+    with example_file.open() as f:
+        example_sheet = json.load(f, object_pairs_hook=collections.OrderedDict)
 
-    def setUp(self):
-        self.t = Timesheet(data_file=self.test_file)
-
-        example_file = pathlib.Path('.', 'tests', 'example.json')
-        with example_file.open() as f:
-            self.example_sheet = json.load(
-                f, object_pairs_hook=collections.OrderedDict
-            )
-
-    def tearDown(self):
-        if self.test_file.exists():
-            self.test_file.unlink()
-
-    def test_find_signed_in(self):
+    def test_find_signed_in(self, timesheet):
         """Find all entries of people that are currently signed in
         (all entries that have a time_in value, but not a time_out value).
         """
-        self.t.sheet = self.example_sheet
-        self.t._update_signed_in()
+        timesheet.sheet = self.example_sheet
+        timesheet._update_signed_in()
         expected_entries = ["2ed2be60-693a-44fe-adc1-2803a674ec9b"]
-        self.assertEqual(self.t.signed_in, expected_entries)
+        assert timesheet.signed_in == expected_entries
 
-    def test_add_signed_in(self):
+    def test_add_signed_in(self, timesheet):
         """User signs in, and they are added to the list of currently
         signed in users.
         """
@@ -89,27 +87,27 @@ class TimesheetTest(unittest.TestCase):
             date="2016-02-17",
             time_in="10:45"
         )
-        self.t.save_entry(e)
-        self.assertIn(e.index, self.t.signed_in)
+        timesheet.save_entry(e)
+        assert e.index in timesheet.signed_in
 
-    def test_remove_signed_in(self):
+    def test_remove_signed_in(self, timesheet):
         """User signs out, and they are removed from the list of currently
         signed in users.
         """
-        self.t.sheet = dict(self.example_sheet)
+        timesheet.sheet = dict(self.example_sheet)
         e = Entry(
             user_id="889870966",
             date="2016-02-17",
             time_in="10:45",
             time_out="13:30"
         )
-        self.t.save_entry(e)
-        self.assertNotIn(e.index, self.t.signed_in)
+        timesheet.save_entry(e)
+        assert e.index not in timesheet.signed_in
 
-    def test_load_entry(self):
+    def test_load_entry(self, timesheet):
         """Initialize an entry object with data from the timesheet."""
-        self.t.sheet = self.example_sheet
-        entry = self.t.load_entry("1f4f10a4-b0c6-43bf-94f4-9ce6e3e204d2")
+        timesheet.sheet = self.example_sheet
+        entry = timesheet.load_entry("1f4f10a4-b0c6-43bf-94f4-9ce6e3e204d2")
         expected_entry = Entry(
             user_id="889870966",
             name="Test",
@@ -118,9 +116,9 @@ class TimesheetTest(unittest.TestCase):
             time_out="13:30",
             index="1f4f10a4-b0c6-43bf-94f4-9ce6e3e204d2"
         )
-        self.assertEqual(entry, expected_entry)
+        assert entry == expected_entry
 
-    def test_save_entry(self):
+    def test_save_entry(self, timesheet):
         """Add an entry to an empty timesheet."""
         e = Entry(
             user_id="889870966",
@@ -130,7 +128,7 @@ class TimesheetTest(unittest.TestCase):
             time_out="13:30",
             index="3b27d0f8-3801-4319-398f-ace18829d150"
         )
-        self.t.save_entry(e)
+        timesheet.save_entry(e)
         expected_sheet = {
             "3b27d0f8-3801-4319-398f-ace18829d150": {
                 "Date": "2016-02-17",
@@ -140,33 +138,33 @@ class TimesheetTest(unittest.TestCase):
                 "User ID": "889870966",
             },
         }
-        self.assertEqual(self.t.sheet, expected_sheet)
+        assert timesheet.sheet == expected_sheet
 
-    def test_remove_entry(self):
+    def test_remove_entry(self, timesheet):
         """Remove an entry from a timesheet with multiple entries."""
         index = "1f4f10a4-b0c6-43bf-94f4-9ce6e3e204d2"
         # dicts are mutable; passed by reference unless explicitly copied:
-        self.t.sheet = dict(self.example_sheet)
-        self.t.remove_entry(index)
-        self.assertFalse(index in self.t.sheet)
+        timesheet.sheet = dict(self.example_sheet)
+        timesheet.remove_entry(index)
+        assert index not in timesheet.sheet
 
-    def test_search_entries(self):
+    def test_search_entries(self, timesheet):
         """Find all entries that contain a matching piece of data."""
-        self.t.sheet = self.example_sheet
-        indices = self.t.search_entries("10:45")
+        timesheet.sheet = self.example_sheet
+        indices = timesheet.search_entries("10:45")
         expected_indices = {
             "1f4f10a4-b0c6-43bf-94f4-9ce6e3e204d2",
             "2ed2be60-693a-44fe-adc1-2803a674ec9b"
         }
-        self.assertEqual(indices, expected_indices)
+        assert indices == expected_indices
 
-    def test_load_sheet(self):
+    def test_load_sheet(self, timesheet):
         """Load a sheet from a file."""
-        self.t.load_sheet(data_file=self.example_file)
-        loaded_sheet = self.t.sheet
-        self.assertEqual(loaded_sheet, self.example_sheet)
+        timesheet.load_sheet(data_file=self.example_file)
+        loaded_sheet = timesheet.sheet
+        assert loaded_sheet == self.example_sheet
 
-    def test_load_invalid_sheet(self):
+    def test_load_invalid_sheet(self, timesheet):
         """Load an invalid json file. Make sure it gets
         renamed a with a '.bak' suffix.
         """
@@ -174,22 +172,13 @@ class TimesheetTest(unittest.TestCase):
         backup = invalid_file.with_suffix('.bak')
         with invalid_file.open('w') as f:
             f.write("invalid file contents")
-        self.t.load_sheet(data_file=invalid_file)
-        self.assertTrue(backup.is_file())
-        self.assertFalse(invalid_file.is_file())
+        timesheet.load_sheet(data_file=invalid_file)
+        assert backup.is_file()
+        assert (invalid_file.is_file()) is False
         backup.unlink()
 
-    def test_save_sheet(self):
+    def test_save_sheet(self, timesheet):
         """Save a sheet to a file."""
-        self.t.sheet = self.example_sheet
-        self.t.save_sheet()
-        self.assertTrue(
-            filecmp.cmp(
-                str(self.t.data_file),
-                str(self.example_file)
-            )
-        )
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+        timesheet.sheet = self.example_sheet
+        timesheet.save_sheet()
+        assert filecmp.cmp(str(timesheet.data_file), str(self.example_file))
